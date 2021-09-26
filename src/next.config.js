646ee19cd736065543eccf2780a570926eca60b0
sina96 <s.bastani96@gmail.com>
2021-09-27 01:01:02 +0200

    Add github Actions

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/sina96/sina96.github.io/gh-pages/' : '',
   reactStrictMode: true,
 }
