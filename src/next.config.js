85bf9d38c3ea8341391252e3d7f89dc478b45b3c
sina96 <s.bastani96@gmail.com>
2021-10-01 14:06:32 +0200

    deleted again

--- src/next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/sina96/sina96.github.io/gh-pages/' : '',
-  reactStrictMode: true,
-}
